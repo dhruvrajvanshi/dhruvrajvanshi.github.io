013573fd0c4bc916e21aa0b4487016173073a7ef
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-07-29 13:15:31 +0200

    Build static pages with markdown

---
+++ build.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+import { readdir, readFile, mkdir, cp, writeFile } from 'node:fs/promises'
+import assert from 'node:assert/strict'
+import { parse } from 'marked'
+import Path from 'node:path'
+import { existsSync } from 'node:fs'
+
+const OUT_DIR = './dist'
+async function main() {
+  await mkdir(OUT_DIR, { recursive: true })
+
+  for (const file of await readdir('./routes')) {
+    assert(file.endsWith('.md') || file.endsWith('.css'))
+    if (file.endsWith('.css')) {
+      cp(Path.resolve('routes', file), Path.resolve(OUT_DIR, file))
+    }
+    const text = await readFile(Path.resolve('routes', file), 'utf8')
+    const parsed = parse(text)
+    const routeName = file.replace(/\.md$/, '')
+    const hasCSS = existsSync(Path.resolve(OUT_DIR, routeName + '.css'))
+    const outPath = Path.resolve(OUT_DIR, routeName + '.html')
+    const cssLink = hasCSS
+      ? `<link rel="stylesheet" href="/${routeName}.css">`
+      : ''
+    const outText = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    ${cssLink}
+</head>
+<body>
+${parsed}
+</body>
+</html>
+`
+    await writeFile(outPath, outText, 'utf8')
+  }
+}
+
+await main()
