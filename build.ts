b088c34c1b711572d6d452013e7934e485a4ffc3
Dhruv Rajvanshi <dhruv@rajv.dev>
2025-07-29 14:00:11 +0200

    Add a base url

--- build.ts
+++ build.ts
@@ -28,6 +28,7 @@ async function main() {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <base href="/">
     ${cssLink}
 </head>
 <body>
